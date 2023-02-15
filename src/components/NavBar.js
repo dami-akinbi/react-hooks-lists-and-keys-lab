476510c01382b51c7140af98890018ef7a4f3e97
damilolaakinbi <akinbidamilola074@gmail.com>
2023-02-15 07:23:27 +0100

    pass props correctly, ensure it rendered correctly

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,18 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const aLinks = links.map((link) => (
+    <a key={link} href={link}>
+      {link}
+    </a>
+  ));
+
+  return (
+    <nav>
+      {/* display an <a> tag for each link here */}
+      {aLinks}
+    </nav>
+  );
 }
 
 export default NavBar;
